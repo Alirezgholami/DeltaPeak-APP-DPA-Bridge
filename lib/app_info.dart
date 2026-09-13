13e44fd6e4876027da1be28989dfa4a05bc38cc4
Alireza_GHolami <153443204+Alirezgholami@users.noreply.github.com>
2026-09-13 21:02:46 +0330

    Bump DPA to v0.5.12+21

--- lib/app_info.dart
+++ lib/app_info.dart
@@ -1,6 +1,6 @@
 class AppInfo {
-  static const appVersion = '0.5.9';
-  static const buildNumber = 17;
+  static const appVersion = '0.5.12';
+  static const buildNumber = 21;
   static const expectedDatabaseVersion = 'DPD v3.90 + v3.91 enrichment B10';
   static const expectedSchemaVersion = '6';
   static const expectedDatasetRevision = 'v390-v391b10-corefields-v2';
